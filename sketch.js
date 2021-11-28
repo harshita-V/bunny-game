97eae61fa4742d519c5f3b2442ea52a8d9c390c0
Harshita <88340671+harshita-V@users.noreply.github.com>
2021-11-28 18:55:23 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,112 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+let engine;
+let world;
+var ground;
+var fruit,rope;
+var fruit_con;
+
+var bg_img;
+ var food;
+var rabbit,bunny;
+var blink,sad,eat;
+
+function preload()
+ { bg_img = loadImage('background.png'); 
+ food = loadImage('melon.png');
+rabbit = loadImage('Rabbit-01.png');
+blink=loadAnimation("blink_1.png","blink_2.png","blink_3.png");
+sad=loadAnimation("sad_1.png","sad_2.png","sad_3.png");
+eat=loadAnimation("eat_0.png","eat_1.png","eat_2.png","eat_3.png","eat_4.png");
+
+blink.playing=true;
+eat.playing=true;
+sad.playing=true;
+sad.looping=false;
+eat.looping=false;
+ }
+
+function setup() 
+{
+  createCanvas(500,700);
+  frameRate(80);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(200,680,600,20);
+
+
+  blink.frameDelay =20;
+  eat.frameDelay =20;
+  sad.frameDelay =20;
+
+   button = createImg('cut_btn.png');
+   button.position(200,30); 
+   button.size(50,50); 
+   button.mouseClicked(drop);
+
+  rope = new Rope(7,{x:245,y:30});
+  fruit = Bodies.circle(300,300,20);
+  Matter.Composite.add(rope.body,fruit);
+
+  bunny=createSprite(250,650,100,100);
+  bunny.addImage(rabbit);
+  bunny.scale=0.2;
+  
+  bunny.addAnimation("blinking",blink);
+  bunny.addAnimation("eating",eat);
+  bunny.addAnimation("crying",sad);
+
+  fruit_con = new Link(rope,fruit);
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+  textSize(50)
+  
+}
+
+function draw() 
+{
+  background(51);
+  image(bg_img,0,0,displayWidth+80,displayHeight);
+  ground.show();
+  rope.show(); 
+
+  push(); 
+  imageMode(CENTER); 
+  if(fruit!=null){ 
+   image(food,fruit.position.x,fruit.position.y,70,70); 
+  }
+   pop(); 
+  Engine.update(engine); 
+
+  if(collide(fruit,bunny)==true)
+  { bunny.changeAnimation('eating'); } 
+  if(collide(fruit,ground.body)==true ) 
+  { bunny.changeAnimation('crying'); }
+  
+drawSprites();
+}
+function drop()
+ { rope.break();
+fruit_con.detach();
+fruit_con = null; }
+
+function Collide(body,sprite){
+  if(body != null){
+     var d=dist(body.position.x,body.position.y,sprite.position.x,sprite.position.y);
+     if(d<=80)
+     { World.remove(engine.world,fruit); 
+      fruit = null; 
+      return true; 
+    } 
+    else{ return false; 
+    }
+  }
+}
